f1f7a199d2f47542d5607c366d81a3fb228b72bc
Jacane-123 <bavajacopo@gmail.com>
2026-08-31 21:10:10 +0200

    Remove unused GLTFLoader import
    Removed unused GLTFLoader import from main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
